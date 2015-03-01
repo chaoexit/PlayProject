93ac16cef630b7316765c5d396d0ea4cf584b183
Tanatorn Assawaamnuey <neen_tta@hotmail.com>
2015-03-01 20:30:41 +0700

    create new vote page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,9 +4,11 @@ import play.*;
 import play.mvc.*;
 
 import result.Vote;
+import scala.collection.immutable.List;
 import views.html.*;
 
 public class Application extends Controller {
     public static Result index() { return ok(index.render("Your new application is ready!!!!")); }
-    public static Result votingresult() { return ok(complete.render(Vote.find.all())); }
+    public static Result votingresult() { return ok(complete.render( Vote.find.all())); }
+    public static Result gotoVotePage() { return ok(vote.render("New vote page"));}
 }
