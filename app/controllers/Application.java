7edb248f5c6691130cb0587326757c37cb8ec68e
Thanyaboon Tovorapan <whatineat@gmail.com>
2015-03-01 22:58:19 +0700

    Fixed star vote to submit to db

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,11 @@
 package controllers;
 
-import play.*;
 import play.mvc.*;
-
 import result.Vote;
-import scala.collection.immutable.List;
 import views.html.*;
 
 public class Application extends Controller {
     public static Result index() { return ok(index.render("Your new application is ready!!!!")); }
-    public static Result votingresult() { return ok(complete.render( Vote.find.all())); }
+    public static Result votingResult() { return ok(complete.render( Vote.find.all())); }
     public static Result gotoVotePage() { return ok(vote.render("New vote page"));}
 }
